8d85a8f409964732c97d98e43251cd8e58abeae5
vhietala <vhietala@gmail.com>
2018-01-16 12:48:24 +0200

    b part

--- Documents/WBMA_course/w1t1/src/app/app.module.ts
+++ Documents/WBMA_course/w1t1/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    MainComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
